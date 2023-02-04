d1751cbca4a2b2eeab2d9270d36d6cef334a6e22
amqkv <amqkvo@gmail.com>
2023-02-04 18:24:26 -0500

    eatdeeznuts

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,15 @@
 import {
   Box, Checkbox, Divider, Image, Select, Switch, Text
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import "./Sidebar.scss";
 import Logo from "../../assets/logo.png"
 import { RiFilter2Fill } from "react-icons/ri";
+import { DocumentData } from "firebase/firestore";
 
-export default function Sidebar() {
+export default function Sidebar(locations: any) {
+  const [filters, setFilters] = useState({ itinerant: true, foodBank: true, shelter: false, needsHygiene: false, hasPet: true })
+  console.log(locations)
   return <Box className="sidebar_container">
     <Box className="header_container">
       <Image src={Logo} alt="Logo" className="logo_img" boxSize="50px" />
@@ -23,35 +26,35 @@ export default function Sidebar() {
         <Divider className="title_divider" sx={{ borderBottomWidth: 1.25, borderBottomColor: "#6e6e6e" }} />
         <Box className="filter_option">
           <Text>Itinerant</Text>
-          <Checkbox checked={true} />
+          <Checkbox isChecked={filters.itinerant} />
         </Box>
         <Box className="divider_container">
           <Divider className="option_divider" />
         </Box>
         <Box className="filter_option">
           <Text>Food Bank</Text>
-          <Checkbox checked={true} />
+          <Checkbox isChecked={filters.foodBank} />
         </Box>
         <Box className="divider_container">
           <Divider className="option_divider" />
         </Box>
         <Box className="filter_option">
           <Text>Shelter</Text>
-          <Checkbox checked={true} />
+          <Checkbox isChecked={filters.shelter} />
         </Box>
         <Box className="divider_container">
           <Divider className="option_divider" />
         </Box>
         <Box className="filter_option">
           <Text>Needs hygiene products</Text>
-          <Checkbox checked={true} />
+          <Checkbox isChecked={filters.needsHygiene} />
         </Box>
         <Box className="divider_container">
           <Divider className="option_divider" />
         </Box>
         <Box className="filter_option">
           <Text>Has a pet</Text>
-          <Checkbox checked={true} />
+          <Checkbox isChecked={filters.hasPet} />
         </Box>
         <Box className="divider_container">
           <Divider className="option_divider" />
