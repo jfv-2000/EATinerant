4e9c800ab61994f76b34ef6eacd8f618fe132c9e
Nicholas <yiphoiye.n@gmail.com>
2023-02-05 02:07:02 -0500

    autodelete record after 24h

---
+++ src/auto-delete.ts
@@ -0,0 +1,22 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const { firestore } = require("firebase-admin");
+admin.initializeApp();
+
+exports.removeExpiredDocuments = functions.pubsub
+  .schedule("every 1 hours")
+  .onRun(async (context: any) => {
+    const db = admin.firestore();
+    const now = firestore.Timestamp.now();
+    const ts = firestore.Timestamp.fromMillis(now.toMillis() - 86400000);
+
+    const snap = await db
+      .collection("docs")
+      .where("publishedAt", "<", ts)
+      .get();
+    let promises: any[] = [];
+    snap.forEach((snap: { ref: { delete: () => any } }) => {
+      promises.push(snap.ref.delete());
+    });
+    return Promise.all(promises);
+  });
