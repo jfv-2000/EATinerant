f042d955bf4548ea43fddcfde41354673bfd65d5
Nicholas <yiphoiye.n@gmail.com>
2023-02-04 22:40:46 -0500

    styling sidebar;

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -33,100 +33,98 @@ export default function Sidebar({
           EATinérant
         </Text>
       </Box>
-      <Box className="sidebar_body">
-        <Box className="sidebar_section">
-          <Box className="sidebar_section_title">
-            <RiFilter2Fill />
-            <Text fontSize="md" className="sidebar_section_title_text">
-              Filters
-            </Text>
-          </Box>
-          <Divider
-            className="title_divider"
-            sx={{ borderBottomWidth: 1.25, borderBottomColor: "#6e6e6e" }}
-          />
-          <Box className="dropdown_option">
-            <Text>Type</Text>
-            <Select
-              defaultValue={filters.type}
-              w="100px"
-              size="sm"
-              onChange={(e) => handleFilterChange(e, "type")}
-            >
-              <option value="itinerant">Itinerant</option>
-              <option value="foodBank">Food Bank</option>
-            </Select>
-          </Box>
-          <Box className="divider_container">
-            <Divider className="option_divider" />
-          </Box>
-          <Box className="dropdown_option">
-            <Text>Needs hygiene products</Text>
-            <Select
-              disabled={filters.type === "foodBank"}
-              defaultValue={filters.needsHygiene}
-              w="115px"
-              size="sm"
-              onChange={(e) => handleFilterChange(e, "needsHygiene")}
-            >
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-              <option value="both">Doesn't matter</option>
-            </Select>
-          </Box>
-          <Box className="divider_container">
-            <Divider className="option_divider" />
-          </Box>
-          <Box className="dropdown_option">
-            <Text>Has a pet</Text>
-            <Select
-              disabled={filters.type === "foodBank"}
-              defaultValue={filters.pet}
-              w="115px"
-              size="sm"
-              onChange={(e) => handleFilterChange(e, "pet")}
-            >
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-              <option value="both">Doesn't matter</option>
-            </Select>
-          </Box>
-          <Box className="divider_container">
-            <Divider className="option_divider" />
-          </Box>
-          <Box className="dropdown_option">
-            <Text>Last fed</Text>
-            <Select
-              disabled={filters.type === "foodBank"}
-              defaultValue={filters.pet}
-              w="100px"
-              size="sm"
-              onChange={(e) => handleFilterChange(e, "lastFed")}
-            >
-              <option value="four">&gt; 4 hrs</option>
-              <option value="eight">&gt; 8 hrs</option>
-              <option value="twelve">&gt; 12 hrs</option>
-              <option value="idc">Doesn't matter</option>
-            </Select>
-          </Box>
-          <Box className="divider_container">
-            <Divider className="option_divider" />
-          </Box>
+      <Box className="sidebar_section">
+        <Box className="sidebar_section_title">
+          <RiFilter2Fill />
+          <Text fontSize="md" className="sidebar_section_title_text">
+            Filters
+          </Text>
         </Box>
-        <Button
-          className="login_button"
-          variant="solid"
-          onClick={() => auth.signOut()}
-          colorScheme="blue"
-          backgroundColor="white"
-          color="#0f83f5"
-          sx={{ border: "#0f83f5 1px solid", borderRadius: "0px" }}
-          _hover={{ color: "white", backgroundColor: "blue.500" }}
-          rightIcon={<FcGoogle />}
-        >
-          Log Out
-        </Button>
+        <Divider
+          className="title_divider"
+          sx={{ borderBottomWidth: 1.25, borderBottomColor: "#6e6e6e" }}
+        />
+        <Box className="dropdown_option">
+          <Text>Type</Text>
+          <Select
+            defaultValue={filters.type}
+            w="100px"
+            size="sm"
+            onChange={(e) => handleFilterChange(e, "type")}
+          >
+            <option value="itinerant">Itinerant</option>
+            <option value="foodBank">Food Bank</option>
+          </Select>
+        </Box>
+        <Box className="divider_container">
+          <Divider className="option_divider" />
+        </Box>
+        <Box className="dropdown_option">
+          <Text>Needs hygiene products</Text>
+          <Select
+            disabled={filters.type === "foodBank"}
+            defaultValue={filters.needsHygiene}
+            w="115px"
+            size="sm"
+            onChange={(e) => handleFilterChange(e, "needsHygiene")}
+          >
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+            <option value="both">Doesn't matter</option>
+          </Select>
+        </Box>
+        <Box className="divider_container">
+          <Divider className="option_divider" />
+        </Box>
+        <Box className="dropdown_option">
+          <Text>Has a pet</Text>
+          <Select
+            disabled={filters.type === "foodBank"}
+            defaultValue={filters.pet}
+            w="115px"
+            size="sm"
+            onChange={(e) => handleFilterChange(e, "pet")}
+          >
+            <option value="yes">Yes</option>
+            <option value="no">No</option>
+            <option value="both">Doesn't matter</option>
+          </Select>
+        </Box>
+        <Box className="divider_container">
+          <Divider className="option_divider" />
+        </Box>
+        <Box className="dropdown_option">
+          <Text>Last fed</Text>
+          <Select
+            disabled={filters.type === "foodBank"}
+            defaultValue={filters.pet}
+            w="100px"
+            size="sm"
+            onChange={(e) => handleFilterChange(e, "lastFed")}
+          >
+            <option value="four">&gt; 4 hrs</option>
+            <option value="eight">&gt; 8 hrs</option>
+            <option value="twelve">&gt; 12 hrs</option>
+            <option value="idc">Doesn't matter</option>
+          </Select>
+        </Box>
+        <Box className="divider_container">
+          <Divider className="option_divider" />
+        </Box>
       </Box>
+      <Button
+        className="login_button"
+        variant="solid"
+        onClick={() => auth.signOut()}
+        colorScheme="blue"
+        backgroundColor="white"
+        color="#0f83f5"
+        sx={{ border: "#0f83f5 1px solid", borderRadius: "0px" }}
+        _hover={{ color: "white", backgroundColor: "blue.500" }}
+        rightIcon={<FcGoogle />}
+      >
+        Log Out
+      </Button>
     </Box>
   );
 }
